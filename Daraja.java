ea952c43969657c66368ce36a725b664e0c668cf
Пользователь <bektur996>
2025-10-02 19:53:59 +0600

    bek

---
+++ Daraja.java
@@ -0,0 +1,18 @@
+public class Daraja{
+	public static void main(String [] args) {
+	  int san = 3;
+	  int Daraja = 4;
+	  int res = 1;
+	  if(Daraja <= 10){
+	     for(int i = 1;i<=Daraja;i++){
+	         res = san*res;
+	         if (i==Daraja);
+         System.out.println(Daraja + "=" + res);
+
+	    }
+	  }else {
+	  	System.out.println("С‡С‹РєРїР°Р№С‚");
+	  }
+
+	}
+}
